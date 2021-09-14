dc1f94d84b891e4325c0cebfd7da0204850e573c
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2021-09-14 22:34:31 +0200

    constexpr-if can help to get collection of etherogenuous types

--- main.cpp
+++ main.cpp
@@ -28,19 +28,6 @@ using light_concept_t = std::variant<light_bulb_t, dimmerable_light_bulb_t, colo
 
 int main(int, char **)
 {
-    // unsupported
-    static_assert(!is_color_light_bulb_v<void>, "`void` should not be a supported color light");
-    static_assert(!is_color_light_bulb_v<int>, "`int` should not be a supported color light");
-    static_assert(!is_color_light_bulb_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_bool_reset_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_void_setFullScale_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_void_setColor_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-
-    // supported
-    static_assert(is_color_light_bulb_v<not_light_but_supported_t>, "`not_light_but_supported_t` should be a supported color light");
-    static_assert(is_color_light_bulb_v<color_light_bulb_t>, "`color_light_bulb_t` should be a supported color light");
-    static_assert(has_void_setColor_v<color_light_bulb_t>, "`color_light_bulb_t` should be a supported color light");
-
     collection<light_concept_t> c;
     c.emplace_back(light_bulb_t());
     c.emplace_back(dimmerable_light_bulb_t());
@@ -48,7 +35,10 @@ int main(int, char **)
     
     std::for_each(c.begin(), c.end(), [](light_concept_t& light_){
         std::visit([](auto&& light) {
-            light.reset();
+            if constexpr (has_bool_reset_v<decltype(light)>)
+            {
+                light.reset();
+            }
         }, light_);
     });
 
