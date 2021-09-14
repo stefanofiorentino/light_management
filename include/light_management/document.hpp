d1f82419cee51043740c653658a3608f678e2fb5
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2021-09-14 22:48:57 +0200

    Initial Commit
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

---
+++ include/light_management/document.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+template <typename T>
+void draw(const T& x, std::ostream& out, size_t position)
+{
+    out << std::string(position, ' ') << x << "\n";
+}
+
+class object_t {
+public: 
+    template <typename T>
+    object_t(T x) : self_(std::make_shared<model_t<T>>(std::move(x))) {}
+    
+    friend void draw(const object_t&x, std::ostream& out, size_t position)
+    { x.self_->draw(out, position); }
+private: 
+    struct concept_t
+    {
+        virtual ~concept_t() = default;
+        virtual void draw(std::ostream& out, size_t position) const = 0;
+    };
+    template <typename T>
+    struct model_t final : concept_t
+    {
+        model_t(T x) : data_(std::move(x)){}
+        void draw(std::ostream& out, size_t position) const override
+        {
+            ::draw(data_, out, position);
+        }
+        T data_;
+    };
+    std::shared_ptr<const concept_t> self_;
+};
+
+using document_t = std::vector<object_t>;
+
+void draw(const document_t& x, std::ostream& out, size_t position)
+{
+    out << std::string(position, ' ') << "<document>\n" ;
+    for (const auto& e: x) draw(e, out, position + 2);
+    out << std::string(position, ' ') << "</document>\n" ;
+}
--- main.cpp
+++ main.cpp
@@ -7,51 +7,32 @@
 #include <light_management/dimmerable_light_bulb.hpp>
 #include <light_management/color_light_bulb.hpp>
 
-struct unsupported_light_t
+#include <light_management/document.hpp>
+
+struct my_class_t {};
+
+void draw(const my_class_t&, std::ostream& out, size_t position)
 {
-    bool setFullScale(float fullScale_) {}
-    void reset() { return; }
-};
+    out << std::string(position, ' ') << "my_class_t" << "\n";
+}
 
-struct not_light_but_supported_t
+void test_helper(std::ostream& out)
+{    
+    document_t document;
+    document.reserve(5);
+
+    document.emplace_back(0);
+    document.emplace_back(std::string("Hello!"));
+    document.emplace_back(document);
+    document.emplace_back(my_class_t());
+    document.emplace_back(4);
+
+    std::reverse(std::begin(document), std::end(document));
+    draw(document, out, 0);
+}
+
+
+int main()
 {
-    void setColor() {}
-    void setFullScale() {}
-    bool reset() { return true; }
-};
-
-struct my_concept_light_t final : light_concept_t
-{
-
-};
-
-template <typename T>
-using collection = std::vector<T>;
-
-int main(int, char **)
-{
-    // unsupported
-    static_assert(!is_color_light_bulb_v<void>, "`void` should not be a supported color light");
-    static_assert(!is_color_light_bulb_v<int>, "`int` should not be a supported color light");
-    static_assert(!is_color_light_bulb_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_bool_reset_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_void_setFullScale_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-    static_assert(!has_void_setColor_v<unsupported_light_t>, "`unsupported_light_t` should not be a supported color light");
-
-    // supported
-    static_assert(is_color_light_bulb_v<not_light_but_supported_t>, "`not_light_but_supported_t` should be a supported color light");
-    static_assert(is_color_light_bulb_v<color_light_bulb_t>, "`color_light_bulb_t` should be a supported color light");
-    static_assert(has_void_setColor_v<color_light_bulb_t>, "`color_light_bulb_t` should be a supported color light");
-
-    collection<std::shared_ptr<light_concept_t>> c;
-    c.emplace_back(std::make_shared<my_concept_light_t>());
-    c.emplace_back(std::make_shared<light_bulb_t>());
-    c.emplace_back(std::make_shared<dimmerable_light_bulb_t>());
-    c.emplace_back(std::make_shared<color_light_bulb_t>());
-    
-    std::for_each(c.begin(), c.end(), [](std::shared_ptr<light_concept_t>& light){
-        light->reset();
-    });
-
-    return EXIT_SUCCESS;
+    test_helper(std::cout);
 }
