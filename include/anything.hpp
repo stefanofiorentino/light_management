5bfcd117e0887939a0246e76d5d9b943ef2f69eb
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2021-10-22 15:38:38 +0000

    `anything' refactoring

---
+++ include/anything.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <utility>
+#include <cassert>
+#include <memory>
+
+// https://www.youtube.com/watch?v=0I0FD3N5cgM
+struct anything {
+  anything() = default;
+  anything(anything const &rhs) : handle_(rhs.handle_->clone()) {}
+  anything &operator=(anything const &rhs) {
+    anything temp(rhs);
+    std::swap(temp, *this);
+    return *this;
+  }
+  template <typename T>
+  anything(T t)
+      : handle_(new handle<typename std::remove_reference<T>::type>(
+            std::forward<T>(t))) {}
+  template <typename T> anything &operator=(T t) {
+    anything temp(std::forward<T>(t));
+    std::swap(temp, *this);
+    return *this;
+  }
+
+  // Public interface
+  int value() const {
+    assert(handle_);
+    return handle_->value();
+  }
+
+private:
+  struct handle_base {
+    virtual ~handle_base() = default;
+    virtual handle_base *clone() const = 0;
+    virtual int value() const = 0;
+  };
+
+  template <typename T> struct handle : handle_base {
+    handle(T value) : value_(std::move(value)) {}
+    virtual handle_base *clone() const { return new handle(value_); }
+    virtual int value() const { return value_.value(); };
+    T value_;
+  };
+
+  std::unique_ptr<handle_base> handle_;
+};
--- test/pragmatic_type_erasure.cpp
+++ test/pragmatic_type_erasure.cpp
@@ -5,47 +5,7 @@
 
 #include <light_management/type_traits/has_int_value.hpp>
 
-// https://www.youtube.com/watch?v=0I0FD3N5cgM
-struct anything {
-  anything() = default;
-  anything(anything const &rhs) : handle_(rhs.handle_->clone()) {}
-  anything &operator=(anything const &rhs) {
-    anything temp(rhs);
-    std::swap(temp, *this);
-    return *this;
-  }
-  template <typename T>
-  anything(T t)
-      : handle_(new handle<typename std::remove_reference<T>::type>(
-            std::forward<T>(t))) {}
-  template <typename T> anything &operator=(T t) {
-    anything temp(std::forward<T>(t));
-    std::swap(temp, *this);
-    return *this;
-  }
-
-  // Public interface
-  int value() const {
-    assert(handle_);
-    return handle_->value();
-  }
-
-private:
-  struct handle_base {
-    virtual ~handle_base() = default;
-    virtual handle_base *clone() const = 0;
-    virtual int value() const = 0;
-  };
-
-  template <typename T> struct handle : handle_base {
-    handle(T value) : value_(std::move(value)) {}
-    virtual handle_base *clone() const { return new handle(value_); }
-    virtual int value() const { return value_.value(); };
-    T value_;
-  };
-
-  std::unique_ptr<handle_base> handle_;
-};
+#include <anything.hpp>
 
 struct foo {
   int value() const { return EXIT_SUCCESS; }
@@ -63,7 +23,8 @@ int value_of() {
 
 TEST(pragmatic_type_erasure, anything) { ASSERT_EQ(EXIT_SUCCESS, value_of()); }
 
-template <typename F, typename... Args> void visit(F f, Args &&... args) {
+template <typename F, typename... Args> 
+void visit(F f, Args &&... args) {
   f(args...);
 }
 
@@ -85,12 +46,13 @@ TEST(pragmatic_type_erasure, visit_with_constexpr_checks_success) {
   ASSERT_EQ(EXIT_SUCCESS, b.value());
 
   auto rc = EXIT_FAILURE;
-  visit([](auto const &v, auto &&rc) {
-    if
-      constexpr(has_int_value_v<std::remove_cv_t<decltype(v)>>) {
-        rc = v.value();
-      }
-  }, f, rc);
+  visit(
+      [](auto const &v, auto &&rc) {
+        if constexpr (has_int_value_v<std::remove_cv_t<decltype(v)>>) {
+          rc = v.value();
+        }
+      },
+      f, rc);
   ASSERT_EQ(EXIT_SUCCESS, rc);
 }
 
