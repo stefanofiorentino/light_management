a6df6ef32348fd9f24d2106bb1c073079b63c64e
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2021-10-30 12:05:50 +0200

    renaming collection into collection_t

--- test/collezioni_polimorfiche_e_dove_trovarle/std_variant.cpp
+++ test/collezioni_polimorfiche_e_dove_trovarle/std_variant.cpp
@@ -11,13 +11,13 @@
 #include <light_management/color_light_bulb.hpp>
 
 template <typename T>
-using collection = std::vector<T>;
+using collection_t = std::vector<T>;
 
 using light_concept_t = std::variant<light_bulb_t, dimmerable_light_bulb_t, color_light_bulb_t>;
 
 TEST(light_management, std_variant)
 {
-    collection<light_concept_t> c;
+    collection_t<light_concept_t> c;
     c.emplace_back(light_bulb_t());
     c.emplace_back(dimmerable_light_bulb_t());
     c.emplace_back(color_light_bulb_t());
