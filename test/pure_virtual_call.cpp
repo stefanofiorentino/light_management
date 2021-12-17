b422c693d232a72e49cd3e128ab96296a54dedea
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2021-12-17 11:22:17 +0100

    pure virtual call
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

---
+++ test/pure_virtual_call.cpp
@@ -0,0 +1,20 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+struct base {
+  virtual ~base() = default;
+  virtual void foo() const = 0;
+};
+
+struct derived final : base {
+  void foo() const {}
+};
+
+TEST(pure_virtual_call, simple) {
+  base *b;
+  {
+    derived d;
+    b = &d;
+  }
+  ASSERT_DEATH({ b->foo(); }, "");
+}
