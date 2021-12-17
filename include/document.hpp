080ae78f70553eb6b06ca9da1a1999a43db37477
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2021-12-17 17:44:52 +0100

    adding do_switch example to test scalability of Sean's idea
    	in the light management specific example
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- include/document.hpp
+++ include/document.hpp
@@ -9,6 +9,9 @@
 template <typename T>
 void draw(const T& x, std::ostream& out, size_t position);
 
+template <typename T>
+void do_switch(T& x, bool status);
+
 class object_t {
 public: 
     template <typename T>
@@ -16,11 +19,15 @@ public:
     
     friend void draw(const object_t&x, std::ostream& out, size_t position)
     { x.self_->draw(out, position); }
+
+    friend void do_switch(object_t&x, bool status)
+    { x.self_->do_switch(status); }
 private: 
     struct concept_t
     {
         virtual ~concept_t() = default;
         virtual void draw(std::ostream& out, size_t position) const = 0;
+        virtual void do_switch(bool) = 0;
     };
     template <typename T>
     struct model_t final : concept_t
@@ -28,11 +35,19 @@ private:
         model_t(T x) : data_(std::move(x)){}
         void draw(std::ostream& out, size_t position) const override
         {
-            ::draw(data_, out, position); 
+          if constexpr (has_void_draw_v<T>) {
+            ::draw(data_, out, position);
+          }
+        }
+        void do_switch(bool status) override
+        {
+            if constexpr (has_void_do_switch_v<T>) {
+                ::do_switch(data_, status);
+            }
         }
         T data_;
     };
-    std::shared_ptr<const concept_t> self_;
+    std::shared_ptr<concept_t> self_;
 };
 
 using collection_t = std::vector<object_t>;
@@ -49,4 +64,16 @@ void draw(const collection_t& x, std::ostream& out, size_t position)
     out << std::string(position, ' ') << "<document>\n" ;
     for (const auto& e: x) draw(e, out, position + 2);
     out << std::string(position, ' ') << "</document>\n" ;
+}
+
+template <typename T>
+void do_switch(T& x, bool status)
+{
+    x.do_switch(status);
+}
+
+template <>
+void do_switch(collection_t& x, bool status)
+{
+    for (auto& e: x) do_switch(e, status);
 }
---
+++ include/light_management/type_traits/has_return_code_do_switch.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <type_traits>
+
+template <typename T, class Enable>
+struct has_void_do_switch_impl : std::false_type
+{
+};
+
+template <typename T>
+struct has_void_do_switch_impl<T, decltype(std::declval<T>().do_switch(false))> : std::true_type
+{
+};
+
+template <typename T>
+struct has_void_do_switch : has_void_do_switch_impl<T, void>
+{
+};
+
+template <class T>
+inline constexpr bool has_void_do_switch_v = has_void_do_switch<T>::value;
