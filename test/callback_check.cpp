1012e9244568cc0b86843b9a694f811175683672
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2021-12-23 17:18:39 +0100

    adding to_be_skipped to signal the callback to avoid actions
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- test/callback_check.cpp
+++ test/callback_check.cpp
@@ -9,18 +9,16 @@
 static uv_timer_t timeout_handle;
 static uv_prepare_t prepare_handle;
 static volatile std::atomic_bool callback_called_with_success;
+static volatile std::atomic_bool to_be_skipped;
 static std::thread th;
-static constexpr std::size_t TIMEOUT = 5;
-static constexpr std::size_t REPEAT = 5;
+static constexpr std::size_t TIMEOUT = 1;
+static constexpr std::size_t REPEAT = 1;
 static constexpr std::size_t TASK_EXECUTION_TIME = 25;
 static constexpr std::size_t SUCCESS_ALLOWED_TASK_EXECUTION_TIME = 50;
-static constexpr std::size_t FAILING_ALLOWED_TASK_EXECUTION_TIME = 15;
+static constexpr std::size_t FAILING_ALLOWED_TASK_EXECUTION_TIME = 5;
 static std::size_t counter = 0;
 
 static void pass_through_cb(uv_handle_t *handle) {}
-static void success_cb(uv_handle_t *handle) {
-  ASSERT_TRUE(atomic_load(&callback_called_with_success));
-}
 static void failing_cb(uv_handle_t *handle) {
   ASSERT_FALSE(atomic_load(&callback_called_with_success));
 }
@@ -29,6 +27,7 @@ static void timeout_cb(uv_timer_t *handle) {
   ASSERT_EQ(handle, &timeout_handle);
   auto &timeout = *static_cast<std::size_t *>(handle->data);
   if ((counter++) * REPEAT > timeout) {
+    atomic_store(&to_be_skipped, true);
     uv_timer_stop(handle);
     if (!uv_is_closing((uv_handle_t *)handle)) {
       uv_close((uv_handle_t *)handle, failing_cb);
@@ -36,12 +35,15 @@ static void timeout_cb(uv_timer_t *handle) {
   } else if (atomic_load(&callback_called_with_success)) {
     uv_timer_stop(handle);
     if (!uv_is_closing((uv_handle_t *)handle)) {
-      uv_close((uv_handle_t *)handle, success_cb);
+      uv_close((uv_handle_t *)handle, pass_through_cb);
     }
   }
 }
 
 static void OnAsyncResult() {
+  if (atomic_load(&to_be_skipped)) {
+    return;
+  }
   atomic_store(&callback_called_with_success, true);
 }
 
@@ -63,7 +65,9 @@ static void prepare_cb(uv_prepare_t *handle) {
 TEST(callback_check, success) {
   int rc;
 
+  counter = 0;
   atomic_store(&callback_called_with_success, false);
+  atomic_store(&to_be_skipped, false);
 
   rc = uv_prepare_init(uv_default_loop(), &prepare_handle);
   ASSERT_TRUE(EXIT_SUCCESS == rc);
@@ -88,7 +92,9 @@ TEST(callback_check, success) {
 TEST(callback_check, timeout) {
   int rc;
 
+  counter = 0;
   atomic_store(&callback_called_with_success, false);
+  atomic_store(&to_be_skipped, false);
 
   rc = uv_prepare_init(uv_default_loop(), &prepare_handle);
   ASSERT_TRUE(EXIT_SUCCESS == rc);
@@ -97,7 +103,7 @@ TEST(callback_check, timeout) {
 
   rc = uv_timer_init(uv_default_loop(), &timeout_handle);
   ASSERT_TRUE(EXIT_SUCCESS == rc);
-  rc = uv_timer_start(&timeout_handle, timeout_cb, 5, 5);
+  rc = uv_timer_start(&timeout_handle, timeout_cb, TIMEOUT, REPEAT);
   ASSERT_TRUE(EXIT_SUCCESS == rc);
   timeout_handle.data =
       const_cast<std::size_t *>(&FAILING_ALLOWED_TASK_EXECUTION_TIME);
@@ -105,6 +111,7 @@ TEST(callback_check, timeout) {
   rc = uv_run(uv_default_loop(), UV_RUN_DEFAULT);
   ASSERT_TRUE(EXIT_SUCCESS == rc);
 
+  ASSERT_FALSE(atomic_load(&callback_called_with_success));
   ASSERT_TRUE(th.joinable());
   th.join();
 }
