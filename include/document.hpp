a8f3221952a42ec9062e844a2dd3d9fd8014869d
Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
2022-04-21 09:00:17 +0000

    fix small SCA warning
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>

--- include/document.hpp
+++ include/document.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using collection_t = std::vector<int>;
-void draw(collection_t const &c, std::ostream &out, size_t position) {
+inline void draw(collection_t const &c, std::ostream &out, size_t position) {
   std::for_each(c.cbegin(), c.cend(), [&out, position](auto&& value){
       out << std::string(position, ' ' ) << value << '\n';
   });
