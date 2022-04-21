d336bf917961b1f5e5332e08cd79ce4bf5668d22
Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
2022-04-21 09:01:27 +0000

    format
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>

--- include/document.hpp
+++ include/document.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using collection_t = std::vector<int>;
 inline void draw(collection_t const &c, std::ostream &out, size_t position) {
-  std::for_each(c.cbegin(), c.cend(), [&out, position](auto&& value){
-      out << std::string(position, ' ' ) << value << '\n';
+  std::for_each(c.cbegin(), c.cend(), [&out, position](auto &&value) {
+    out << std::string(position, ' ') << value << '\n';
   });
 }
