909d77811f75492d6222665f35cdd9609fb728b7
Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
2022-04-22 14:04:06 +0000

    how to add a custom collection item type

--- test/collezioni_polimorfiche_e_dove_trovarle/runtime_polymorphism.cpp
+++ test/collezioni_polimorfiche_e_dove_trovarle/runtime_polymorphism.cpp
@@ -2,6 +2,14 @@
 
 #include <document.hpp> // https://www.youtube.com/watch?v=QGcVXgEVMJg
 
+struct my_struct_t {
+  char const *const name = "my_struct_t";
+};
+template <>
+void draw(my_struct_t const &x, std::ostream &out, size_t position) {
+  out << std::string(position, ' ') << x.name << "\n";
+}
+
 TEST(runtime_polymorphism, draw_plain_plant) {
   collection_t c;
   c.emplace_back(42);
@@ -9,7 +17,7 @@ TEST(runtime_polymorphism, draw_plain_plant) {
   c.emplace_back(s);
 
   c.emplace_back(c);
-
+  c.emplace_back(my_struct_t());
   std::ostringstream oss;
   draw(c, oss, 0);
   ASSERT_EQ(R"(<collection>
@@ -19,6 +27,7 @@ TEST(runtime_polymorphism, draw_plain_plant) {
     42
     a_string
   </collection>
+  my_struct_t
 </collection>
 )",
             oss.str());
