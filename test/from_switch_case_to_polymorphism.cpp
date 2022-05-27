eb0739e281867480136f33999d6764342da48c0a
Stefano Fiorentino <stefanofiorentino@users.noreply.github.com>
2022-05-27 16:32:47 +0200

    Feature/from switch case to polymorphism (#33)
    * new test case for experimentation
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
    
    * C-style polymorphism
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
    
    * Cpp_style_polymorphism
    
    	the only reflection currently available in cpp, kind-of
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
    
    * avoid __PRETTY_FUNCTION__
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
    
    * use std::string instead of char*
    
    Co-authored-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>

---
+++ test/from_switch_case_to_polymorphism.cpp
@@ -0,0 +1,51 @@
+#include <gmock/gmock.h>
+
+#include <cstring>
+#include <memory>
+#include <string>
+
+namespace details {
+std::string print_foo() { return "char* details::print_foo()"; }
+std::string print_bar() { return "char* details::print_bar()"; }
+} // namespace details
+
+std::string perform_action_based_on_type(std::string const &action_to_perform) {
+  if ("foo" == action_to_perform) {
+    return details::print_foo();
+  } else if ("bar" == action_to_perform) {
+    return details::print_bar();
+  }
+}
+
+TEST(from_switch_case_to_polymorphism, C_style_polymorphism) {
+  ASSERT_EQ("char* details::print_foo()", perform_action_based_on_type("foo"));
+  ASSERT_EQ("char* details::print_bar()", perform_action_based_on_type("bar"));
+}
+
+class base {
+protected:
+  virtual std::string do_print() const = 0;
+
+public:
+  std::string print() const { return do_print(); }
+};
+class foo : public base {
+  std::string do_print() const override { return details::print_foo(); }
+};
+
+class bar : public base {
+  std::string do_print() const override { return details::print_bar(); }
+};
+
+std::shared_ptr<base> factory(std::string const &action_to_perform) {
+  if ("foo" == action_to_perform) {
+    return std::make_shared<foo>();
+  } else if ("bar" == action_to_perform) {
+    return std::make_shared<bar>();
+  }
+}
+
+TEST(from_switch_case_to_polymorphism, Cpp_style_polymorphism) {
+  ASSERT_EQ("char* details::print_foo()", factory("foo")->print());
+  ASSERT_EQ("char* details::print_bar()", factory("bar")->print());
+}
