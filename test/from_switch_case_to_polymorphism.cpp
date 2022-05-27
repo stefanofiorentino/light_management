5a4876968b98269f00ca396a458d3d500acfeeed
Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
2022-05-27 14:15:55 +0000

    Cpp_style_polymorphism
    	the only reflection currently available in cpp, kind-of
    
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>

--- test/from_switch_case_to_polymorphism.cpp
+++ test/from_switch_case_to_polymorphism.cpp
@@ -1,6 +1,7 @@
 #include <gmock/gmock.h>
 
 #include <cstring>
+#include <memory>
 #include <string>
 
 namespace details {
@@ -21,4 +22,34 @@ TEST(from_switch_case_to_polymorphism, C_style_polymorphism) {
                       perform_action_based_on_type("foo")));
   ASSERT_EQ(0, strcmp("char* details::print_bar()",
                       perform_action_based_on_type("bar")));
+}
+
+class base {
+protected:
+  virtual char *do_print() const = 0;
+
+public:
+  char *print() const { return do_print(); }
+};
+class foo : public base {
+  char *do_print() const override { return details::print_foo(); }
+};
+
+class bar : public base {
+  char *do_print() const override { return details::print_bar(); }
+};
+
+std::shared_ptr<base> factory(std::string const &action_to_perform) {
+  if ("foo" == action_to_perform) {
+    return std::make_shared<foo>();
+  } else if ("bar" == action_to_perform) {
+    return std::make_shared<bar>();
+  }
+}
+
+TEST(from_switch_case_to_polymorphism, Cpp_style_polymorphism) {
+  ASSERT_EQ(0, strcmp("char* details::print_foo()",
+                      factory("foo")->print()));
+  ASSERT_EQ(0, strcmp("char* details::print_bar()",
+                      factory("bar")->print()));
 }
