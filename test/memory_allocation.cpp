f547302d1091df9435959be1b398c9ee700843ee
Fiorentino Ing. Stefano <stefano.fiorentino@adesso.ch>
2022-07-14 17:25:57 +0000

    to be presented

--- test/memory_allocation.cpp
+++ test/memory_allocation.cpp
@@ -4,75 +4,71 @@
 #include <vector>
 #include <tuple>
 
-void *operator new(std::size_t) = delete;
-// {
-//     std::puts(__PRETTY_FUNCTION__);
-//     throw std::bad_alloc();
-// }
-void *operator new(std::size_t, std::nothrow_t) noexcept = delete;
-// {
-//     std::puts(__PRETTY_FUNCTION__);
-//     std::terminate();
-// }
-void *operator new(std::size_t, std::align_val_t) = delete;
-// {
-//     std::puts(__PRETTY_FUNCTION__);
-//     throw std::bad_alloc();
-// }
-void *operator new(std::size_t, std::align_val_t, std::nothrow_t) noexcept = delete;
-// {
-//     std::puts(__PRETTY_FUNCTION__);
-//     std::terminate();
-// }
-
-// cppreference> std::vector is a sequence container that encapsulates dynamic size arrays
-// RAII 
-// TEST(memory_allocation_vector, memory_allocation_vector)
-// {
-//     std::vector<int> v{10,0};
-//     std::cout << v.size() << std::endl;
-// }
-
-// TEST(memory_allocation_array, memory_allocation_array)
-// {
-//     std::array<int, 10> v;
-//     std::cout << v.size() << std::endl;
-// }
-
-// TEST(memory_allocation_shared_ptr, memory_allocation_shared_ptr)
-// {
-//     int* p = static_cast<int*>(malloc(sizeof(int)));
-//     std::shared_ptr<int> v(p);
-// }
+#define START try{
+#define END }catch(...){}
+void *operator new(std::size_t)
+{
+    std::puts(__PRETTY_FUNCTION__);
+    throw std::bad_alloc();
+}
+void *operator new(std::size_t, std::nothrow_t) noexcept 
+{
+    std::puts(__PRETTY_FUNCTION__);
+    std::terminate();
+}
+void *operator new(std::size_t, std::align_val_t)
+{
+    std::puts(__PRETTY_FUNCTION__);
+    throw std::bad_alloc();
+}
+void *operator new(std::size_t, std::align_val_t, std::nothrow_t) noexcept
+{
+    std::puts(__PRETTY_FUNCTION__);
+    std::terminate();
+}
 
 void destroy(int*) {
     std::puts(__PRETTY_FUNCTION__);
 }
 
 int main() {
-    // std::vector<int> v;
-    // std::cout << v.size() << std::endl;
 
-    // std::array<int, 10> v;
-    // std::cout << v.size() << std::endl;
-    
-    // int* p = static_cast<int*>(malloc(sizeof(int)));
-    // std::shared_ptr<int> v(p);
+START
+    std::vector<int> v;
+    std::cout << v.size() << std::endl;
+END
 
-    // int* p = static_cast<int*>(malloc(sizeof(int)));
-    // std::unique_ptr<int> v1(p);
-    // std::cout << sizeof(v1) << std::endl;
+START
+    std::array<int, 10> v;
+    std::cout << v.size() << std::endl;
+END
 
-    // std::unique_ptr<int, void(*)(int*)> v2(p, destroy);
-    // std::cout << sizeof(v2) << std::endl;
+START
+    int* p = static_cast<int*>(malloc(sizeof(int)));
+    std::shared_ptr<int> v(p);
+END
 
-    // int a = 0;
-    // auto f = [&a](int*){
-    //     std::puts(__PRETTY_FUNCTION__);
-    // };
-    // std::unique_ptr<int, decltype(f)> v(p, f);
-    // std::cout << sizeof(v) << std::endl;
+START
+    int* p = static_cast<int*>(malloc(sizeof(int)));
+    std::unique_ptr<int> v(p);
+    std::cout << sizeof(v) << std::endl;
+END
 
+START
+    int* p = static_cast<int*>(malloc(sizeof(int)));
+    std::unique_ptr<int, void(*)(int*)> v(p, destroy);
+    std::cout << sizeof(v) << std::endl;
+END
+
+START
+    int* p = static_cast<int*>(malloc(sizeof(int)));
+    int a = 0;
+    auto f = [&a](int*){
+        std::puts(__PRETTY_FUNCTION__);
+    };
+    std::unique_ptr<int, decltype(f)> v(p, f);
+    std::cout << sizeof(v) << std::endl;
+END
     // TODO: ...
     // std::tuple<int, double> v;
     // v = 10;
