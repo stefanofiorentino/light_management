0cf28220a4360963aead5916ea849a31b1fd396e
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-09-22 10:18:09 +0200

    format

--- test/sdk_abstraction.cpp
+++ test/sdk_abstraction.cpp
@@ -2,16 +2,15 @@
 
 #include <string>
 
-struct DirectiveSequencerInterface
-{
-    virtual ~DirectiveSequencerInterface() = default;
-    virtual void onDirective(const std::string &directive) const = 0;
+struct DirectiveSequencerInterface {
+  virtual ~DirectiveSequencerInterface() = default;
+  virtual void onDirective(const std::string &directive) const = 0;
 };
 
-struct DirectiveSequencer final : public DirectiveSequencerInterface { // check 2 classi final
-    virtual ~DirectiveSequencer() = default; // check 1 distruttore virtuale
-    void onDirective(const std::string &directive) const override
-  {
+struct DirectiveSequencer final
+    : public DirectiveSequencerInterface { // check 2 classi final
+  virtual ~DirectiveSequencer() = default; // check 1 distruttore virtuale
+  void onDirective(const std::string &directive) const override {
     // precondition
     std::puts(__PRETTY_FUNCTION__);
     // NOT HERE!!!
@@ -23,42 +22,31 @@ struct DirectiveSequencer final : public DirectiveSequencerInterface { // check 
 // best case scenario
 // TODO......... DirectiveSequencer::onDirective(...)
 
-
-
 // worst case scenario
-struct DirectiveSequencerWrapper : public DirectiveSequencerInterface
-{
-    DirectiveSequencerWrapper(DirectiveSequencer& directiveSequencer)
-        : m_directiveSequencer(directiveSequencer)
-    {
-
-    }
-    void onDirective(const std::string &directive) const override
-  {
+struct DirectiveSequencerWrapper : public DirectiveSequencerInterface {
+  DirectiveSequencerWrapper(DirectiveSequencer &directiveSequencer)
+      : m_directiveSequencer(directiveSequencer) {}
+  void onDirective(const std::string &directive) const override {
     // precondition
     m_directiveSequencer.onDirective(directive);
     // postcondition
   }
-  private:
-  DirectiveSequencer& m_directiveSequencer;
-};
-//===================================================
-struct SampleApp
-{
-    SampleApp(DirectiveSequencer& directiveSequencer)
-    : m_directiveSequencer(directiveSequencer)
-    {
-
-    }
 
 private:
-    DirectiveSequencer& m_directiveSequencer;
+  DirectiveSequencer &m_directiveSequencer;
+};
+//===================================================
+struct SampleApp {
+  SampleApp(DirectiveSequencer &directiveSequencer)
+      : m_directiveSequencer(directiveSequencer) {}
+
+private:
+  DirectiveSequencer &m_directiveSequencer;
 };
 
-TEST(sdk_abstraction, test)
-{
-    DirectiveSequencer directiveSequencer;
-    DirectiveSequencerWrapper directiveSequencerWrapper(directiveSequencer);
-    SampleApp sampleApp(directiveSequencer);
-    directiveSequencer.onDirective("");
+TEST(sdk_abstraction, test) {
+  DirectiveSequencer directiveSequencer;
+  DirectiveSequencerWrapper directiveSequencerWrapper(directiveSequencer);
+  SampleApp sampleApp(directiveSequencer);
+  directiveSequencer.onDirective("");
 }
