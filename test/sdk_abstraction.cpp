62be4305746d2bf1da45fb777fe5fcf2885aae46
Stefano Fiorentino <stefanofiorentino@users.noreply.github.com>
2022-09-24 00:31:00 +0200

    Nadir 2 (#40)
    * remove broken repos/experiments
    
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
    
    * sdk_abstraction
    
    * format
    
    * minors
    
    * build fix thsan
    
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
    Co-authored-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

---
+++ test/sdk_abstraction.cpp
@@ -0,0 +1,49 @@
+#include <gmock/gmock.h>
+
+#include <string>
+
+struct DirectiveSequencerInterface {
+  virtual ~DirectiveSequencerInterface() = default;
+  virtual void onDirective(const std::string &directive) const = 0;
+};
+
+struct DirectiveSequencer final
+    : public DirectiveSequencerInterface { // check 2 classi final
+  virtual ~DirectiveSequencer() = default; // check 1 distruttore virtuale
+  void onDirective(const std::string &directive) const override {
+    std::puts(__PRETTY_FUNCTION__);
+  }
+};
+
+// best case scenario
+// TODO......... DirectiveSequencer::onDirective(...)
+
+// worst case scenario
+struct DirectiveSequencerWrapper : public DirectiveSequencerInterface {
+  DirectiveSequencerWrapper(DirectiveSequencerInterface &directiveSequencer)
+      : m_directiveSequencer(directiveSequencer) {}
+  void onDirective(const std::string &directive) const override {
+    // precondition
+    m_directiveSequencer.onDirective(directive);
+    // postcondition
+  }
+
+private:
+  DirectiveSequencerInterface &m_directiveSequencer;
+};
+//===================================================
+struct SampleApp {
+  SampleApp(DirectiveSequencerInterface &directiveSequencer)
+      : m_directiveSequencer(directiveSequencer) {
+    m_directiveSequencer.onDirective("");
+  }
+
+private:
+  DirectiveSequencerInterface &m_directiveSequencer;
+};
+
+TEST(sdk_abstraction, test) {
+  DirectiveSequencer directiveSequencer;
+  DirectiveSequencerWrapper directiveSequencerWrapper(directiveSequencer);
+  SampleApp sampleApp(directiveSequencerWrapper);
+}
