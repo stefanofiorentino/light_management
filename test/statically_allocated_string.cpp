b471928f54755c615b028e6ea72a18173f18b2a2
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-10-07 23:37:36 +0200

    adding example with std::map

--- test/statically_allocated_string.cpp
+++ test/statically_allocated_string.cpp
@@ -1,5 +1,6 @@
 #include <gmock/gmock.h>
 
+#include <map>
 #include <scoped_allocator>
 #include <string>
 #include <utility>
@@ -15,6 +16,8 @@ template <typename T>
 using SA1024 = std::scoped_allocator_adaptor<ArenaAllocator<T, Arena1024>>;
 
 using astring = std::basic_string<char, std::char_traits<char>, SA1024<char>>;
+template <typename K, typename V>
+using amap = std::map<K, V, std::less<>, SA1024<std::pair<const K, V>>>;
 
 astring make_astring(std::string const &rhs,
                      ArenaAllocator1024 &arenaAllocator) {
@@ -27,4 +30,12 @@ TEST(statically_allocated_string, whenStdStringIsUsedThenItWorks) {
   auto str =
       make_astring(experimental::constants::VERY_LONG_STRING, arenaAllocator);
   ASSERT_EQ(experimental::constants::VERY_LONG_STRING, str.c_str());
+}
+
+TEST(statically_allocated_string, whenStdMapIsUsedTHenItWorks) {
+  Arena1024 arena;
+  ArenaAllocator1024 arenaAllocator(&arena);
+  amap<int, astring> m(arenaAllocator);
+  m[42] = experimental::constants::VERY_SHORT_STRING;
+  ASSERT_EQ(experimental::constants::VERY_SHORT_STRING, m.at(42).c_str());
 }
