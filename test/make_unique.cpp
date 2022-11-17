7d6034d41cc7fe7488fbbb062bbedc7dfb414a8c
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-11-17 11:05:30 +0100

    clang-format

--- test/make_unique.cpp
+++ test/make_unique.cpp
@@ -6,5 +6,5 @@ TEST(make_unique, simple)
 {
   constexpr std::size_t N = 10;
   auto ptr = std::make_unique<const uint8_t[]>(N);
-  assert(sizeof(ptr)==sizeof(uint8_t*));
+  assert(sizeof(ptr) == sizeof(uint8_t*));
 }
