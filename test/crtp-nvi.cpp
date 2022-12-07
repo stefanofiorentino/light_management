883829a0a37cb99227f80958a2d9010280ed23fe
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-12-07 17:19:25 +0100

    more clean solutio up-to-me

--- test/crtp-nvi.cpp
+++ test/crtp-nvi.cpp
@@ -3,11 +3,13 @@
 template<typename T>
 class wrap
 {
+
   std::string to_string_pre() const { return "pre-"; }
 
   std::string to_string_post() const { return "-post"; }
 
 public:
+  friend T;
   std::string to_string() const
   {
     return to_string_pre() + static_cast<const T*>(this)->to_string_impl() +
@@ -17,8 +19,8 @@ public:
 
 class foo final : public wrap<foo>
 {
-public:
   std::string to_string_impl() const { return "inside"; }
+  friend class wrap<foo>;
 };
 
 TEST(crtp, nvi)
