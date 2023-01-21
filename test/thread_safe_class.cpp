3c756ba39faf0badb27ab06ce1686962610c67e3
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-01-21 15:14:48 +0100

    example of detecting data race with THSAN
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

---
+++ test/thread_safe_class.cpp
@@ -0,0 +1,56 @@
+#include <gmock/gmock.h>
+
+#include <future>
+
+class thread_safe
+{
+  bool status;
+  // mutable std::mutex mtx; // concurrency got wrong by Sean Parent
+
+public:
+  thread_safe()
+    : status(false)
+  {}
+  bool getStatus() const
+  {
+    // std::lock_guard<std::mutex> lg{ mtx };
+    return status;
+  }
+
+  void setStatus(bool status)
+  {
+    // std::lock_guard<std::mutex> lg{ mtx };
+    thread_safe::status = status;
+  }
+};
+
+TEST(thread_safe, basic)
+{
+  thread_safe ts;
+  ASSERT_FALSE(ts.getStatus());
+}
+
+// concurrency by Scott Meyers
+template<typename F, typename... Args>
+inline auto
+reallyAsync(F&& f, Args&&... args)
+{
+  return std::async(
+    std::launch::async, std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+TEST(thread_safe, thsan)
+{
+  thread_safe ts;
+  bool status{};
+
+  ASSERT_FALSE(status);
+
+  auto th1 =
+    reallyAsync([](thread_safe& ts) { ts.setStatus(true); }, std::ref(ts));
+
+  auto th2 = reallyAsync(
+    [](thread_safe const& ts, bool& status) { status = ts.getStatus(); },
+    ts,
+    std::ref(status));
+}
