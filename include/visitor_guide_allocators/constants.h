ac0d8c92800e5d6fadb9f100caad525c4161c25d
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-02-25 15:08:22 +0100

    change tests to check memmov is actually working
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

--- include/visitor_guide_allocators/constants.h
+++ include/visitor_guide_allocators/constants.h
@@ -6,6 +6,9 @@ namespace experimental {
 namespace constants {
 static std::string const& VERY_SHORT_STRING = "short";
 static std::string const& VERY_LONG_STRING = "Very looooooooooooooong string";
+static std::string const& VERY_LONG_STRING_1 = "Very looooooooooooooong string 1";
+static std::string const& VERY_LONG_STRING_2 = "Very looooooooooooooong string 2";
+static std::string const& VERY_LONG_STRING_3 = "Very looooooooooooooong string 3";
 static char const* const VERY_LONG_STRING_C_STR = VERY_LONG_STRING.c_str();
 static std::string const& VERY_LONG_STRING_CAUSING_EXCEPTION = R"({
     "string": "Very looooooooooooooong string, Very looooooooooooooong string, Very looooooooooooooong string"
