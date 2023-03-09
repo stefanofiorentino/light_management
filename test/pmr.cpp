f3df0bff35959e1660846c1b1ed382b00c927bec
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-03-09 11:13:53 +0100

    change test name to more meaningful one
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

--- test/pmr.cpp
+++ test/pmr.cpp
@@ -9,10 +9,26 @@ TEST(pmr, vector_string)
   ASSERT_EQ("first", pmr_vector_string.front());
 }
 
-TEST(pmr, chatGPT)
+TEST(pmr, monotonic_buffer_resource)
 {
   // Arrange
   std::pmr::monotonic_buffer_resource pool{ 1024 };
+  std::pmr::polymorphic_allocator<char> alloc{ &pool };
+  std::pmr::vector<std::pmr::string> vec{ alloc };
+
+  // Act
+  vec.emplace_back("Hello, ");
+  vec.emplace_back("world!");
+
+  // Assert
+  ASSERT_EQ("Hello, world!", vec.front() + vec.back());
+}
+
+TEST(pmr, synchronized_pool_resource)
+{
+  // Arrange
+  std::pmr::monotonic_buffer_resource buffer{ 1024 };
+  std::pmr::synchronized_pool_resource pool{ &buffer };
   std::pmr::polymorphic_allocator<char> alloc{ &pool };
   std::pmr::vector<std::pmr::string> vec{ alloc };
 
