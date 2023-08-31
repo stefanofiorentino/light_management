cfee117d9eeaecfe75582a7bb2e38cf3858bc904
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-08-31 19:58:08 +0200

    dump

--- test/unoffensive_shared_ptr.cpp
+++ test/unoffensive_shared_ptr.cpp
@@ -5,35 +5,35 @@
 #include <memory>
 #include <numeric>
 
+template<typename T>
 class unoffensive_shared_ptr
 {
-  std::shared_ptr<int> sp;
+  std::shared_ptr<T> sp;
 
-  int* get_raw_ptr_or_default() const
+  T* get_raw_ptr_or_default() const
   {
     if (!sp) {
-      static auto local_sp =
-        std::make_shared<int>(std::numeric_limits<int>::min());
-      return local_sp.get();
+      static T t{};
+      return &t;
     }
     return sp.get();
   }
 
 public:
-  explicit unoffensive_shared_ptr(std::shared_ptr<int> const& sp)
+  explicit unoffensive_shared_ptr(std::shared_ptr<T> const& sp)
     : sp(sp)
   {}
-  int* get() const noexcept { return get_raw_ptr_or_default(); }
+  T* get() const noexcept { return get_raw_ptr_or_default(); }
 
-  int& operator*() const noexcept { return *get_raw_ptr_or_default(); }
+  T& operator*() const noexcept { return *get_raw_ptr_or_default(); }
 
-  int* operator->() const noexcept { return get_raw_ptr_or_default(); }
+  T* operator->() const noexcept { return get_raw_ptr_or_default(); }
 };
 
 TEST(unoffensive_shared_ptr, pointer_to_one)
 {
-  std::shared_ptr<int> sp = std::make_shared<int>(1);
-  unoffensive_shared_ptr usp{ sp };
+  auto sp = std::make_shared<int>(1);
+  unoffensive_shared_ptr<int> usp{ sp };
   EXPECT_EQ(1, *usp.get());
   EXPECT_EQ(1, usp.operator*());
   EXPECT_EQ(1, *usp.operator->());
@@ -42,8 +42,38 @@ TEST(unoffensive_shared_ptr, pointer_to_one)
 TEST(unoffensive_shared_ptr, return_default_if_nullptr)
 {
   std::shared_ptr<int> sp = nullptr;
-  unoffensive_shared_ptr usp{ sp };
-  EXPECT_EQ(std::numeric_limits<int>::min(), *usp.get());
-  EXPECT_EQ(std::numeric_limits<int>::min(), usp.operator*());
-  EXPECT_EQ(std::numeric_limits<int>::min(), *usp.operator->());
+  unoffensive_shared_ptr<int> usp{ sp };
+  EXPECT_EQ(0, *usp.get());
+  EXPECT_EQ(0, usp.operator*());
+  EXPECT_EQ(0, *usp.operator->());
+}
+
+TEST(unoffensive_shared_ptr, pointer_to_char)
+{
+  auto sp = std::make_shared<char>('c');
+  unoffensive_shared_ptr<char> usp{ sp };
+  EXPECT_EQ('c', *usp.get());
+  EXPECT_EQ('c', usp.operator*());
+  EXPECT_EQ('c', *usp.operator->());
+}
+
+TEST(unoffensive_shared_ptr, pointer_to_valid_std__string)
+{
+  constexpr auto expected = "Hello, World!";
+  auto sp = std::make_shared<std::string>(expected);
+  unoffensive_shared_ptr<std::string> usp{ sp };
+  EXPECT_EQ(expected, *usp.get());
+  EXPECT_EQ(expected, usp.operator*());
+  EXPECT_EQ(expected, *usp.operator->());
+  EXPECT_EQ(13, usp->size());
+}
+
+TEST(unoffensive_shared_ptr, pointer_to_unvalid_std__string)
+{
+  std::shared_ptr<std::string> sp;
+  unoffensive_shared_ptr<std::string> usp{ sp };
+  EXPECT_EQ("", *usp.get());
+  EXPECT_EQ("", usp.operator*());
+  EXPECT_EQ("", *usp.operator->());
+  EXPECT_EQ(0, usp->size());
 }
