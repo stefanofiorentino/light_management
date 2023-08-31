137a37f64acf69dbb5c79542832e8e2cea8a45e5
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2023-08-31 18:33:23 +0200

    build related fixes

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . \( -iname *.h -o -iname *.hpp -o -iname *.cpp \) | xargs -r -n1 -I '{}' clang-format --fallback-style=Mozilla -i {}
+find . \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" \) | xargs -r -n1 -I '{}' clang-format-10 --fallback-style=Mozilla -i {}
