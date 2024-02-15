2ae61d56fb7b27ee862a0d5172752f1a60b0ee8e
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2024-02-15 10:10:03 +0000

    fix formatting
    Signed-off-by: Stefano Fiorentino <stefano.fiorentino@adesso.ch>

--- test/overload_set_lambda.cpp
+++ test/overload_set_lambda.cpp
@@ -3,34 +3,45 @@
 
 // https://gist.github.com/socantre/4164652
 
-template <class... Fs> struct overload_set;
+template<class... Fs>
+struct overload_set;
 
-template <class F>
+template<class F>
 struct overload_set<F> : F
 {
-    overload_set(F &&x) : F(std::forward<F>(x)) {}
-    using F::operator();
+  overload_set(F&& x)
+    : F(std::forward<F>(x))
+  {
+  }
+  using F::operator();
 };
 
-template <class F, class... Fs>
-struct overload_set<F, Fs...> : F, overload_set<Fs...>
+template<class F, class... Fs>
+struct overload_set<F, Fs...>
+  : F
+  , overload_set<Fs...>
 {
-    overload_set(F &&x, Fs &&... xs) : F(std::forward<F>(x)), overload_set<Fs...>(std::forward<Fs>(xs)...) {}
-    using F::operator();
-    using overload_set<Fs...>::operator();
+  overload_set(F&& x, Fs&&... xs)
+    : F(std::forward<F>(x))
+    , overload_set<Fs...>(std::forward<Fs>(xs)...)
+  {
+  }
+  using F::operator();
+  using overload_set<Fs...>::operator();
 };
 
-template <class... Fs>
-overload_set<Fs...> overload(Fs &&... xs)
+template<class... Fs>
+overload_set<Fs...>
+overload(Fs&&... xs)
 {
-    return overload_set<Fs...>(std::forward<Fs>(xs)...);
+  return overload_set<Fs...>(std::forward<Fs>(xs)...);
 }
 
-int main() {
-	auto o = overload(
-		[](int){ std::printf("Hello, "); },
-		[](double){ std::printf("World!\n"); }
-	);
-	o(1);
-	o(1.);
+int
+main()
+{
+  auto o = overload([](int) { std::printf("Hello, "); },
+                    [](double) { std::printf("World!\n"); });
+  o(1);
+  o(1.);
 }
