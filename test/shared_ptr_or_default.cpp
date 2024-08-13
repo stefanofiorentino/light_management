a988a2cb36fd1aa93c42a3573840e4babc5f712a
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2024-08-13 17:37:01 +0000

    EXPECT to ASSERT

--- test/shared_ptr_or_default.cpp
+++ test/shared_ptr_or_default.cpp
@@ -34,27 +34,27 @@ TEST(shared_ptr_or_default, pointer_to_one)
 {
   auto sp = std::make_shared<int>(1);
   shared_ptr_or_default<int> usp{ sp };
-  EXPECT_EQ(1, *usp.get());
-  EXPECT_EQ(1, usp.operator*());
-  EXPECT_EQ(1, *usp.operator->());
+  ASSERT_EQ(1, *usp.get());
+  ASSERT_EQ(1, usp.operator*());
+  ASSERT_EQ(1, *usp.operator->());
 }
 
 TEST(shared_ptr_or_default, return_default_if_nullptr)
 {
   std::shared_ptr<int> sp = nullptr;
   shared_ptr_or_default<int> usp{ sp };
-  EXPECT_EQ(0, *usp.get());
-  EXPECT_EQ(0, usp.operator*());
-  EXPECT_EQ(0, *usp.operator->());
+  ASSERT_EQ(0, *usp.get());
+  ASSERT_EQ(0, usp.operator*());
+  ASSERT_EQ(0, *usp.operator->());
 }
 
 TEST(shared_ptr_or_default, pointer_to_char)
 {
   auto sp = std::make_shared<char>('c');
   shared_ptr_or_default<char> usp{ sp };
-  EXPECT_EQ('c', *usp.get());
-  EXPECT_EQ('c', usp.operator*());
-  EXPECT_EQ('c', *usp.operator->());
+  ASSERT_EQ('c', *usp.get());
+  ASSERT_EQ('c', usp.operator*());
+  ASSERT_EQ('c', *usp.operator->());
 }
 
 TEST(shared_ptr_or_default, pointer_to_valid_std__string)
@@ -62,18 +62,18 @@ TEST(shared_ptr_or_default, pointer_to_valid_std__string)
   constexpr auto expected = "Hello, World!";
   auto sp = std::make_shared<std::string>(expected);
   shared_ptr_or_default<std::string> usp{ sp };
-  EXPECT_EQ(expected, *usp.get());
-  EXPECT_EQ(expected, usp.operator*());
-  EXPECT_EQ(expected, *usp.operator->());
-  EXPECT_EQ(13, usp->size());
+  ASSERT_EQ(expected, *usp.get());
+  ASSERT_EQ(expected, usp.operator*());
+  ASSERT_EQ(expected, *usp.operator->());
+  ASSERT_EQ(13, usp->size());
 }
 
 TEST(shared_ptr_or_default, pointer_to_unvalid_std__string)
 {
   std::shared_ptr<std::string> sp;
   shared_ptr_or_default<std::string> usp{ sp };
-  EXPECT_EQ("", *usp.get());
-  EXPECT_EQ("", usp.operator*());
-  EXPECT_EQ("", *usp.operator->());
-  EXPECT_EQ(0, usp->size());
+  ASSERT_EQ("", *usp.get());
+  ASSERT_EQ("", usp.operator*());
+  ASSERT_EQ("", *usp.operator->());
+  ASSERT_EQ(0, usp->size());
 }
